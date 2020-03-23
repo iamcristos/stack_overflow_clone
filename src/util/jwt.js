f3262ea60d4d7b40259d0f76a3bdcb039005ca43
iamcristos <nmereginivincent@gmail.com>
2020-03-23 19:07:51 +0100

    added secrets

--- src/util/jwt.js
+++ src/util/jwt.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import secret from '../config/secret';
 
 function generateToken(user) {
   // eslint-disable-next-line no-underscore-dangle
@@ -9,7 +10,7 @@ function generateToken(user) {
 async function validateToken(token, res) {
   if (!token) return res.status(400).send('token is required');
   try {
-    const { data } = await jwt.verify(token, 'secret');
+    const { data } = await jwt.verify(token, secret.JWT_SECRET);
     return data;
   } catch (error) {
     return res.status(401).send(error);
